11117a598a0b21991845ace0baabc367a657fe6d
vitaliy <bliznuuk@gmail.com>
2018-02-27 20:44:21 +0200

    Create functions partOfSpeech(perent) and syntax(perent) from existing lines of code

--- macmilian.js
+++ macmilian.js
@@ -1,13 +1,19 @@
+var result;
 
 var headbar = document.getElementById("headbar");
-var partOfSpeech = headbar.getElementsByClassName("PART-OF-SPEECH")[0].innerText;
-partOfSpeech;
+partOfSpeech(headbar);
 
-/*var syntax = headbar.getElementsByClassName("SYNTAX-CODING show_less")[0];
-syntax.childNodes[1].textContent;
-*/
 var content = document.getElementById("leftContent").getElementsByClassName("senses")[0].getElementsByTagName("li");
 content;
 
 var def = content[0].getElementsByClassName("DEFINITION")[0];
 def.textContent;
+
+function partOfSpeech(perent){
+  var partOfSpeech = perent.getElementsByClassName("PART-OF-SPEECH")[0].innerText;
+}
+
+function syntax(perent){
+  var syntax = perent.getElementsByClassName("SYNTAX-CODING show_less")[0];
+  syntax.childNodes[1].textContent;
+}

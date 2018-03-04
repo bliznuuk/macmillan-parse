a8ae2e0ce3810f5f18f12b6be90a0358c388b830
vitaliy <bliznuuk@gmail.com>
2018-03-04 19:41:05 +0200

    Now it get from page: word, definition and syntax from it. Implement functions: word(), parseLi(li), checkDefinition(body), appendToResult(text). Modify functions: partOfSpeech(parent),from syntax(parent) to checkSyntax(body). In main: move definition block to checkDefinition(body), remove get content line.

--- macmilian.js
+++ macmilian.js
@@ -1,19 +1,65 @@
-var result;
+var result = "";
+
+word();
 
 var headbar = document.getElementById("headbar");
 partOfSpeech(headbar);
 
-var content = document.getElementById("leftContent").getElementsByClassName("senses")[0].getElementsByTagName("li");
-content;
 
-var def = content[0].getElementsByClassName("DEFINITION")[0];
-def.textContent;
+var liTags = document.getElementById("leftContent").getElementsByClassName("senses")[0].getElementsByTagName("li");
+for(i = 0; i < liTags.length; i++){
+  var lisChild = liTags[i].firstChild;
+  parseLi(lisChild);
+};
 
-function partOfSpeech(perent){
-  var partOfSpeech = perent.getElementsByClassName("PART-OF-SPEECH")[0].innerText;
+
+
+function word(){
+  var span = document.getElementById("headwordleft").getElementsByClassName("BASE")[0];
+  var word = span.textContent;
+  appendToResult(word);
 }
 
-function syntax(perent){
-  var syntax = perent.getElementsByClassName("SYNTAX-CODING show_less")[0];
-  syntax.childNodes[1].textContent;
+function partOfSpeech(parent){
+  var partOfSpeech = parent.getElementsByClassName("PART-OF-SPEECH")[0].innerText;
+  appendToResult(partOfSpeech);
+}
+
+function checkSyntax(body){
+  var syntax = body.getElementsByClassName("SYNTAX-CODING")[0];
+  if (syntax != null && syntax.parentNode == body){
+    appendToResult(syntax.childNodes[1].textContent);
+  }
+}
+
+function checkDefinition(body){
+  var definition = body.getElementsByClassName("DEFINITION")[0];
+  if (definition.parentNode == body){
+    appendToResult(definition.textContent);
+  }
+}
+
+function parseLi(li){
+  var num = li.getElementsByClassName("SENSE-NUM")[0].textContent;
+  var separator;
+  var body;
+  if (li.className == "SENSE"){
+    separator = "___";
+    body = li.getElementsByClassName("SENSE-BODY")[0];
+  }
+  if (li.className == "SUB-SENSE-BODY"){
+    separator = "_~__";
+    body = li.getElementsByClassName("SUB-SENSE-CONTENT")[0];
+  }
+  appendToResult(separator + num);
+  
+  checkSyntax(body);
+  checkDefinition(body);
+}
+
+
+function appendToResult(text){
+  text = text.trim();
+  console.log(text);
+  result += text + "\n";
 }
